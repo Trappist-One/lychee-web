192af5a8aa90996da23c8dcbe486c8997ef2c201
= <=>
2023-08-21 22:48:06 +0800

    header

--- src/ui/layout/Header.jsx
+++ src/ui/layout/Header.jsx
@@ -1,4 +1,5 @@
-import { Avatar, Popper } from "@mui/material";
+import { Avatar, Popper, Fade, IconButton } from "@mui/material";
+import { Settings } from "@mui/icons-material";
 
 import { useState } from "react";
 
@@ -15,13 +16,20 @@ export default function Header() {
   };
 
   const canBeOpen = open && Boolean(anchorEl);
-  const id = canBeOpen ? 'transition-popper' : undefined;
+  const id = canBeOpen ? "transition-popper" : undefined;
 
   return (
     <>
       <div className="h-12 shadow-md bg-white w-full p-2">
         <div className=" bg-red-300 w-full h-full flex items-center justify-end gap-x-2">
-          <label onClick={handleClick()} className=" cursor-pointer" aria-describedby={id}>
+          <IconButton aria-label="delete" size="small">
+            <Settings />
+          </IconButton>
+          <label
+            onClick={handleClick()}
+            className=" cursor-pointer"
+            aria-describedby={id}
+          >
             <Avatar
               className=" border w-8 h-8"
               src="https://api.dicebear.com/6.x/adventurer/svg?seed=Abby"
@@ -29,27 +37,20 @@ export default function Header() {
           </label>
 
           <Popper
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        placement="bottom"
-        transition
-      >
-        <div>qqq</div>
-      </Popper>
-
-          <Avatar
-            className=" border w-8 h-8"
-            src="https://api.dicebear.com/6.x/adventurer/svg?seed=Abby"
-          ></Avatar>
-          <Avatar
-            className=" border w-8 h-8"
-            src="https://api.dicebear.com/6.x/adventurer/svg?seed=Abby"
-          ></Avatar>
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            placement="bottom"
+            transition
+          >
+            {({ TransitionProps }) => (
+              <Fade {...TransitionProps}>
+                <div>11</div>
+              </Fade>
+            )}
+          </Popper>
         </div>
       </div>
-
-
     </>
   );
 }
--- src/ui/layout/Sidebar.jsx
+++ src/ui/layout/Sidebar.jsx
@@ -3,9 +3,11 @@ import { menuDataList } from "./data";
 
 export default function Silder() {
   const [open, setOpen] = useState(true);
+  const [showSub, setShowSub] = useState(true);
+  const width = showSub ? " w-[180px]" : " w-[90px]";
 
   return (
-    <div className="w-[180px] h-full shadow-md bg-white flex flex-col">
+    <div className={"h-full shadow-md bg-white flex flex-col" + width}>
       <div className=" h-10 bg-white">
         <img
           className="h-10 w-8 m-auto"
@@ -13,13 +15,17 @@ export default function Silder() {
         ></img>
       </div>
       <div className=" w-4/5 h-[2px] bg-red-200 mx-auto"></div>
-      <MenuList dataList={menuDataList}></MenuList>
+      <MenuList
+        dataList={menuDataList}
+        showSub={showSub}
+        setShowSub={setShowSub}
+      ></MenuList>
     </div>
   );
 }
 
 const MenuList = (prop) => {
-  const dataList = prop.dataList
+  const dataList = prop.dataList;
   const [subMenuDataList, setSubMenuDataList] = useState(
     dataList[0]?.chridList
   );
@@ -28,11 +34,14 @@ const MenuList = (prop) => {
     setSubMenuDataList(
       dataList.filter((menuData) => menuData.id == menuId)[0].chridList
     );
+    if (!prop.showSub) {
+      prop.setShowSub(true);
+    }
   };
 
   return (
-    <div className="w-[180px] flex h-screen text-xs overflow-hidden">
-      <div className="w-1/2  flex flex-col shadow-lg">
+    <div className="w-[180px] flex h-screen text-xs">
+      <div className="w-[90px] max-w-[90px] flex flex-col shadow-lg">
         <ul>
           {dataList.map((menuData) => {
             return (
@@ -47,13 +56,26 @@ const MenuList = (prop) => {
           })}
         </ul>
       </div>
-      <div className="w-1/2 flex flex-col h-screen">
+
+      <div
+        className={
+          "w-[90px] max-w-[90px] flex flex-col h-screen" + prop.showSub ? "" : " hidden"
+        }
+      >
         <ul>
           {subMenuDataList.map((menuData) => {
             if (menuData.title == true) {
-              return <li key={menuData.id} className="px-4 pt-4 text-blue-300">{menuData.menuName}</li>;
+              return (
+                <li key={menuData.id} className="px-4 pt-4 text-blue-300">
+                  {menuData.menuName}
+                </li>
+              );
             } else {
-              return <li key={menuData.id} className="px-4 pt-4 cursor-pointer">{menuData.menuName}</li>;
+              return (
+                <li key={menuData.id} className="px-4 pt-4 cursor-pointer">
+                  {menuData.menuName}
+                </li>
+              );
             }
           })}
         </ul>
