62e7d0e731b39d57664dff620b10328bb32b28cc
= <=>
2023-09-06 22:54:35 +0800

    login layout

--- src/ui/components/Logo.jsx
+++ src/ui/components/Logo.jsx
@@ -5,9 +5,7 @@ export default function Logo(prop) {
         width: prop.size + 'px',
         height: prop.size + 'px'
     }
-
-    console.log(style);
-
+    
     return (<>
         <img src="assets/icons/logo.svg" style={style} className="cursor-pointer">
         </img>
--- src/ui/pages/login/Login.jsx
+++ src/ui/pages/login/Login.jsx
@@ -1,24 +1,48 @@
 import { useTranslation } from "react-i18next";
 import Logo from "../../components/Logo";
-
+import { Button, TextField } from "@mui/material";
 
 export default function Login() {
-    const {t} = useTranslation();
-    return (
-        <div className="flex h-full w-full bg-white">
-            <div className="flex h-full w-full flex-col px-10 pt-10 pb-2">
-                <div >
-                <Logo size="32"></Logo>
-                </div>
-                <div className="h-32 w-full flex items-center justify-center text-3xl">
-                    {t('嗨，欢迎回来！')}
-                </div>
+  const { t } = useTranslation();
+  return (
+    <div className="flex h-full w-full bg-white">
+      <div className="flex h-full w-full flex-col px-10 pt-10 pb-2 bg-[url('assets/icons/login_undraw.svg')] bg-no-repeat background-blur-md">
+        <div className="">
+          <div>
+            <Logo size="32"></Logo>
+          </div>
+          <div className="h-32 w-full flex items-center justify-center text-3xl">
+            {t("嗨，欢迎回来！")}
+          </div>
+          <div className="mt-20 ">
+            <div className="flex items-center justify-center ">
+              <img
+                src="assets/icons/login_undraw.svg"
+                className="h-60 w-full"
+              ></img>
             </div>
-            <div className="h-full w-[600px] bg-red-300 px-5">
-                <div className="w-full mt-64 bg-ly-primary">
-                    
-                </div>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="h-full w-[600px] bg-red-300 px-5 flex items-center">
+        <div className="w-full h-[500px]">
+          <div className=" grid grid-cols-1 gap-8">
+            <div className=" text-center text-white">
+               LYCHEE MANAGE SYSTEM
+            </div>
+            <TextField id="outlined-basic" label="username" />
+            <TextField
+              id="outlined-password-input"
+              label="password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <div className="flex  flex-row-reverse">
+            <Button variant="contained">Login</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
